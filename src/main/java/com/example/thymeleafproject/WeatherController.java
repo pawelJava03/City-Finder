b511b225bef2c0e05c966731e0fecb5e63072fbf
Paweł <pa.st.a@wp.pl>
2023-11-04 13:13:26 +0100

    v1.3 default city + changeable city

--- src/main/java/com/example/thymeleafproject/WeatherController.java
+++ src/main/java/com/example/thymeleafproject/WeatherController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -16,19 +18,25 @@ public class WeatherController {
 
     @GetMapping("/weather")
     public String getWeather(@RequestParam(name = "city", required = false, defaultValue = "Warsaw") String city, Model model) {
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
-        WeatherData weatherData = restTemplate.getForObject(url, WeatherData.class);
+       try {
+           String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+           WeatherData weatherData = restTemplate.getForObject(url, WeatherData.class);
 
-        double temperatureInCelsius = weatherData.getMain().getTemp() - 273.15;
-        double roundedTemperature = Math.round(temperatureInCelsius * 10.0) / 10.0;
+           double temperatureInCelsius = weatherData.getMain().getTemp() - 273.15;
+           double roundedTemperature = Math.round(temperatureInCelsius * 10.0) / 10.0;
 
-        String weatherType = weatherData.getWeather().get(0).getMain();
-        model.addAttribute("weatherType", weatherType);
+           String weatherType = weatherData.getWeather().get(0).getMain();
+           model.addAttribute("weatherType", weatherType);
 
-        model.addAttribute("city", city);
-        model.addAttribute("temperature", roundedTemperature);
-        model.addAttribute("windSpeed", weatherData.getWind().getSpeed());
+           model.addAttribute("city", city);
+           model.addAttribute("temperature", roundedTemperature);
+           model.addAttribute("windSpeed", weatherData.getWind().getSpeed());
 
-        return "weather";
+           return "weather";
+       }catch (HttpClientErrorException e){
+           return "cityNotFound";
+       }catch (RestClientException e){
+           return "apiError";
+       }
     }
 }
