b9cab7732a48caa2380adb0efca9c8b27e7b93f4
Paweł <pa.st.a@wp.pl>
2023-11-05 00:26:10 +0100

    City Finder v1.0

--- src/main/java/com/example/thymeleafproject/WeatherController.java
+++ src/main/java/com/example/thymeleafproject/WeatherController.java
@@ -1,6 +1,12 @@
 package com.example.thymeleafproject;
 
 import com.example.thymeleafproject.readAPI.KeyReader;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,25 +24,48 @@ public class WeatherController {
 
     @GetMapping("/weather")
     public String getWeather(@RequestParam(name = "city", required = false, defaultValue = "Warsaw") String city, Model model) {
-       try {
-           String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
-           WeatherData weatherData = restTemplate.getForObject(url, WeatherData.class);
+        try {
+            String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+            WeatherData weatherData = restTemplate.getForObject(url, WeatherData.class);
 
-           double temperatureInCelsius = weatherData.getMain().getTemp() - 273.15;
-           double roundedTemperature = Math.round(temperatureInCelsius * 10.0) / 10.0;
+            double temperatureInCelsius = weatherData.getMain().getTemp() - 273.15;
+            double roundedTemperature = Math.round(temperatureInCelsius * 10.0) / 10.0;
 
-           String weatherType = weatherData.getWeather().get(0).getMain();
-           model.addAttribute("weatherType", weatherType);
+            String weatherType = weatherData.getWeather().get(0).getMain();
+            model.addAttribute("weatherType", weatherType);
 
-           model.addAttribute("city", city);
-           model.addAttribute("temperature", roundedTemperature);
-           model.addAttribute("windSpeed", weatherData.getWind().getSpeed());
+            model.addAttribute("city", city);
+            model.addAttribute("temperature", roundedTemperature);
+            model.addAttribute("windSpeed", weatherData.getWind().getSpeed());
 
-           return "weather";
-       }catch (HttpClientErrorException e){
-           return "cityNotFound";
-       }catch (RestClientException e){
-           return "apiError";
-       }
+            String apiUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=true&titles="+city;
+            String response = restTemplate.getForObject(apiUrl, String.class);
+
+            // Parsuj odpowiedź JSON
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(response);
+
+            // Pobierz opis Warszawy
+            JsonNode pages = jsonNode.path("query").path("pages");
+            String firstPageKey = pages.fieldNames().next();
+            String cityDescription = pages.path(firstPageKey).path("extract").asText();
+
+            // Usuń znaczniki HTML i formatuj tekst
+            Document doc = Jsoup.parse(cityDescription);
+            String plainText = doc.text();
+
+            // Dodaj sformatowany tekst do modelu
+            model.addAttribute("cityInfo", plainText);
+
+            return "weather";
+        }catch (HttpClientErrorException e){
+            return "cityNotFound";
+        }catch (RestClientException e){
+            return "apiError";
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
